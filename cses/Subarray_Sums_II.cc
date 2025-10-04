591e3cf6526eca036e27bb19588ef2f96b837058
neznajko <neznajko@yahoo.com>
2025-10-04 10:05:35 +0300

    totient

---
+++ cses/Subarray_Sums_II.cc
@@ -0,0 +1,247 @@
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# include <bits/stdc++.h>
+////////////////////////////////////////////////////////////////
+using namespace std;
+////////////////////////////////////////////////////////////////
+template <typename T>
+using v = vector<T>;
+template <typename T>
+using w = v<v<T>>;
+template <typename K, typename V>
+using p = pair<K,V>;
+template <typename T>
+using d = deque<T>;
+template <typename K, typename V>
+using umap = unordered_map<K,V>;
+template <typename T>
+using uset = unordered_set<T>;
+template <typename T, typename Compare=less<T>>
+using maxpq = priority_queue<T, v<T>, Compare>;
+template <typename T, typename Compare=greater<T>>
+using minpq = priority_queue<T, v<T>, Compare>;
+# ifdef __GNUC__
+# include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <typename T, typename Compare=less<T>>
+using iset = tree<T, null_type, Compare, rb_tree_tag,
+                  tree_order_statistics_node_update>;
+# define FBO find_by_order
+# define OOK order_of_key
+# endif
+////////////////////////////////////////////////////////////////
+using i32 = int;
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+using b8l = bool;
+using str = string;
+using chr = char;
+using dbl = double;
+////////////////////////////////////////////////////////////////
+using vi32 = v<i32>;
+using vu32 = v<u32>;
+using vi64 = v<i64>;
+using vu64 = v<u64>;
+using vb8l = v<b8l>;
+using pi32 = p<i32,i32>;
+using pu32 = p<u32,u32>;
+using pi64 = p<i64,i64>;
+using pu64 = p<u64,u64>;
+////////////////////////////////////////////////////////////////
+constexpr chr nl = '\n';
+constexpr chr sp = ' ';
+constexpr chr tab = '\t';
+constexpr u64 mod1 = 1'000'000'007;
+constexpr i32 mini32 = numeric_limits<i32>::min();
+constexpr i32 maxi32 = numeric_limits<i32>::max();
+constexpr i64 mini64 = numeric_limits<i64>::min();
+constexpr i64 maxi64 = numeric_limits<i64>::max();
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+inline void vrooom() {
+    cin.tie( nullptr )->sync_with_stdio( false );
+}
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+istream& operator>>( istream& is, p<K,V>& p ){
+    return is >> p.first >> p.second;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+istream& operator>>( istream& is, v<T>& v ){
+    for( auto& y: v ){ is >> y; }
+    return is;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline T load_scalar() {
+    T y; cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline v<T> load_vector( u32 n ){
+    v<T> y( n ); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline w<T> load_tensor( u32 n, u32 m ){
+    w<T> y( n, v<T>( m )); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename C>
+auto acc( const C& c ){
+    using T = typename C::value_type;
+    return accumulate( c.begin(), c.end(), T( 0 ));
+}
+////////////////////////////////////////////////////////////////
+struct {
+    str open_bracket;
+    str close_bracket;
+    str separator;
+} CONT{ "[", "]", "," },
+  PAIR{ "(", ")", ":" };
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# define FOR_THE_HORDE auto t = load_scalar<u32>(); while( t-- )
+# define PSH push_back
+# define POP pop_back
+# define EMP emplace_back
+# define ENQ push_back
+# define DEQ pop_front
+# define MKP make_pair
+# define FST first
+# define SND second
+# define LB lower_bound
+# define UB upper_bound
+# define odd( x ) ((x) & 1)
+# define evn( y ) (!odd( y ))
+# define sqr( x ) ((x) * (x))
+# define all( x ) x.begin(), x.end()
+# define siz( y ) (u32) y.size()
+# define dbg( x ) cerr << #x << " = " << x << nl
+# define dmp( y ) cout << y << nl;
+# define key( p ) p.first
+# define val( p ) p.second
+# define on( y ) all( y )
+////////////////////////////////////////////////////////////////
+# define OVERLOAD_OSTREAM_OPERATOR( Container )                \
+template <typename... T>                                       \
+ostream& operator<<( ostream& os, const Container<T...>& con ){\
+    os << CONT.open_bracket;                                   \
+    str sep = "";                                              \
+    for( const auto& y: con ){                                 \
+        os << sep << y;                                        \
+        sep = CONT.separator;                                  \
+    }                                                          \
+    return os << CONT.close_bracket;                           \
+}
+////////////////////////////////////////////////////////////////
+OVERLOAD_OSTREAM_OPERATOR( vector );
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+ostream& operator<<( ostream& os, const p<K,V>& p ){
+    return os << PAIR.open_bracket
+              << key( p )
+              << PAIR.separator
+              << val( p )
+              << PAIR.close_bracket;
+}
+////////////////////////////////////////////////////////////////
+OVERLOAD_OSTREAM_OPERATOR( set );
+OVERLOAD_OSTREAM_OPERATOR( unordered_map );
+OVERLOAD_OSTREAM_OPERATOR( deque );
+OVERLOAD_OSTREAM_OPERATOR( unordered_set );
+OVERLOAD_OSTREAM_OPERATOR( map );
+////////////////////////////////////////////////////////////////
+# define dmp2( a, b ) cout << a << sp << b << nl;
+# define dbg2( a, b ) dbg( a ); dbg( b )
+# define dbg3( a, b, c ) dbg2( a, b ); dbg( c )
+# define dbg4( a, b, c, d ) dbg3( a, b, c ); dbg( d )
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+class Algorithm {
+public:
+    Algorithm( i64 n, i64 x, vi64& a ):
+        n( n ),
+        x( x ),
+        a( a )
+    {}
+    // sum a[i..j] = pref[j] - pref[i - 1] = x
+    // pref[j] = x + pref[i - 1]
+    void pusk() {
+        vi64 pref( n + 1 );
+        for( i64 i = 0; i < n; ++i ){
+            pref[i + 1] = a[i] + pref[i];
+        }
+        i64 c = 0;
+        if( x == 0 ){ // nasty critter
+            sort( on( pref ));
+            i64 k = 1;
+            for( i64 i = 1; i <= n; i++ ){
+                if( pref[i] == pref[i - 1] ){
+                    k++;
+                } else {
+                    c += k * (k - 1) / 2;
+                    k = 1;
+                }
+            }
+            c += k * (k - 1) / 2;
+            dmp( c );
+            return;
+        }
+        umap<i64,vi64> f;
+        for( i64 i = 0; i < n; ++i ){
+            f[pref[i + 1]].PSH( i );
+        }
+        for( i64 i = 0; i < n; ++i ){
+            auto y = x + pref[i];
+            if( f.count( y )){
+                auto off = f[y];
+                auto it = LB( on( off ), i );
+                c += off.end() - it;
+            }
+        }
+        dmp( c );
+    }
+private:
+    const i64 n;
+    const i64 x;
+    vi64& a;
+
+};
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+int main() {
+    vrooom();
+    if( 0 ){
+    } else {
+        auto n = load_scalar<i64>();
+        auto x = load_scalar<i64>();
+        auto a = load_vector<i64>( n );
+        Algorithm( n, x, a ).pusk();
+    }
+}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////

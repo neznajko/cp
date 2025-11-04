3dba5961332955aadd3fbd1cf4c1ac031747cbdc
neznajko <neznajko@yahoo.com>
2025-11-04 09:08:54 +0200

    van Dyck's algorithm

---
+++ cses/Cycle_Finding.cc
@@ -0,0 +1,331 @@
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//# define DeBug
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# include <bits/stdc++.h>
+////////////////////////////////////////////////////////////////
+using namespace std;
+////////////////////////////////////////////////////////////////
+template <typename T>
+using v = vector<T>;
+template <typename T>
+using w = v<v<T>>;
+template <typename K, typename V>
+using p = pair<K,V>;
+template <typename T>
+using d = deque<T>;
+template <typename T>
+using e = tuple<T,T,T>;
+template <typename K, typename V>
+using umap = unordered_map<K,V>;
+template <typename T>
+using uset = unordered_set<T>;
+template <typename T>
+using mset = multiset<T>;
+template <typename T, typename Compare=less<T>>
+using maxpq = priority_queue<T, v<T>, Compare>;
+template <typename T, typename Compare=greater<T>>
+using minpq = priority_queue<T, v<T>, Compare>;
+# ifdef __GNUC__
+# include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <typename T, typename Compare=less<T>>
+using iset = tree<T, null_type, Compare, rb_tree_tag,
+                  tree_order_statistics_node_update>;
+# define FBO find_by_order
+# define OOK order_of_key
+# endif
+////////////////////////////////////////////////////////////////
+using i32 = int;
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+using b8l = bool;
+using str = string;
+using chr = char;
+using dbl = double;
+////////////////////////////////////////////////////////////////
+using vi32 = v<i32>;
+using vu32 = v<u32>;
+using vi64 = v<i64>;
+using vu64 = v<u64>;
+using vb8l = v<b8l>;
+using pi32 = p<i32,i32>;
+using pu32 = p<u32,u32>;
+using pi64 = p<i64,i64>;
+using pu64 = p<u64,u64>;
+using ei64 = e<i64>;
+////////////////////////////////////////////////////////////////
+constexpr chr nl = '\n';
+constexpr chr sp = ' ';
+constexpr chr tab = '\t';
+constexpr u64 mod1 = 1'000'000'007;
+constexpr i64 inf = 1'000'000'000'000'000;
+constexpr i32 mini32 = numeric_limits<i32>::min();
+constexpr i32 maxi32 = numeric_limits<i32>::max();
+constexpr i64 mini64 = numeric_limits<i64>::min();
+constexpr i64 maxi64 = numeric_limits<i64>::max();
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+inline void vrooom() {
+    cin.tie( nullptr )->sync_with_stdio( false );
+}
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+istream& operator>>( istream& is, p<K,V>& p ){
+    return is >> p.first >> p.second;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+istream& operator>>( istream& is, e<T>& t) {
+    return is >> get<0>( t ) >> get<1>( t ) >> get<2>( t );
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+istream& operator>>( istream& is, v<T>& v ){
+    for( auto& y: v ){ is >> y; }
+    return is;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline T load_scalar() {
+    T y; cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline v<T> load_vector( u32 n ){
+    v<T> y( n ); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline w<T> load_tensor( u32 n, u32 m ){
+    w<T> y( n, v<T>( m )); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename C>
+auto acc( const C& c ){
+    using T = typename C::value_type;
+    return accumulate( c.begin(), c.end(), T( 0 ));
+}
+////////////////////////////////////////////////////////////////
+struct {
+    str open_bracket;
+    str close_bracket;
+    str separator;
+} CONT{ "[", "]", "," },
+  PAIR{ "(", ")", ":" };
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# define FOR_THE_HORDE auto t = load_scalar<u32>(); while( t-- )
+# define loop while( true )
+# define psh push_back
+# define kpop pop_back
+# define emp emplace_back
+# define in push_back
+# define out pop_front
+# define mkp make_pair
+# define fst first
+# define snd second
+# define L lower_bound
+# define U upper_bound
+# define For( i, begin, end ) for( i64 i = begin; i < end; ++i )
+# define Rev( i, end, begin ) for( i64 i = end; i > begin; --i )
+# define odd( x ) ((x) & 1)
+# define evn( y ) (!odd( y ))
+# define sqr( x ) ((x) * (x))
+# define all( x ) x.begin(), x.end()
+# define siz( y ) (i64) y.size()
+# ifdef DeBug
+#   define dbg( x ) cerr << #x << " = " << x << nl
+# else
+#   define dbg( x )
+# endif
+# define dmp( y ) cout << y << nl
+# define key( p ) p.first
+# define val( p ) p.second
+# define on( y ) all( y )
+////////////////////////////////////////////////////////////////
+# define OVERLOAD_OSTREAM_OPERATOR( Container )                \
+template <typename... T>                                       \
+ostream& operator<<( ostream& os, const Container<T...>& con ){\
+    os << CONT.open_bracket;                                   \
+    str sep = "";                                              \
+    for( const auto& y: con ){                                 \
+        os << sep << y;                                        \
+        sep = CONT.separator;                                  \
+    }                                                          \
+    return os << CONT.close_bracket;                           \
+}
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+ostream& operator<<( ostream& os, const p<K,V>& p );          
+////////////////////////////////////////////////////////////////
+template <typename T>
+ostream& operator<<( ostream& os, const e<T>& t );
+////////////////////////////////////////////////////////////////
+OVERLOAD_OSTREAM_OPERATOR( vector );
+OVERLOAD_OSTREAM_OPERATOR( set );
+OVERLOAD_OSTREAM_OPERATOR( multiset );
+OVERLOAD_OSTREAM_OPERATOR( unordered_map );
+OVERLOAD_OSTREAM_OPERATOR( deque );
+OVERLOAD_OSTREAM_OPERATOR( unordered_set );
+OVERLOAD_OSTREAM_OPERATOR( map );
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+ostream& operator<<( ostream& os, const p<K,V>& p ){
+    return os << PAIR.open_bracket
+              << key( p )
+              << PAIR.separator
+              << val( p )
+              << PAIR.close_bracket;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+ostream& operator<<( ostream& os, const e<T>& t) {
+    return os << "{" << get<0>( t )
+              << "," << get<1>( t )
+              << "," << get<2>( t )
+              << "}";
+}
+////////////////////////////////////////////////////////////////
+# define dmp2( a, b ) cout << a << sp << b << nl;
+# define dbg2( a, b ) dbg( a ); dbg( b )
+# define dbg3( a, b, c ) dbg2( a, b ); dbg( c )
+# define dbg4( a, b, c, d ) dbg3( a, b, c ); dbg( d )
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+class Algorithm {
+public:
+    Algorithm( i64 n, v<ei64>& e ):
+        n(n), e(e),
+        W( w<i64>( n + 1, vi64( n + 1, inf ))),
+        adj( w<i64>( n + 1 )),
+        src( v<bool>( n + 1, true )),
+        dist( vi64( n + 1, inf )),
+        parent( vi64( n + 1 )),
+        count( vi64( n + 1 ))
+    {
+        sort( on( e ));
+        dbg2( n, e );
+        for( auto [u,v,w]: e ){
+            W[u][v] = min( W[u][v], w );
+            if( !adj[u].empty() and adj[u].back() == v ){
+                continue;
+            }
+            adj[u].psh( v );
+            src[v] = false;
+        }
+        src[0] = false;
+        dbg2( adj, src );
+        For( u, 1, n ){
+            if( src[u] ){
+                dist[u] = 0;
+                h.push({ dist[u], u });
+            }
+        }
+    }
+    void pusk() {
+        if( dijk()){ return; }
+        loop {
+            auto m = 0;
+            i64 u;
+            For( v, 1, n + 1 ){
+                if( dist[v] == inf ){
+                    ++m;
+                    u = v;
+                }
+            }
+            if( m == 0 ){ break; }
+            n = m;
+            dist[u] = 0;
+            h.push({ dist[u], u });
+            if( dijk()){ return; }
+        }
+        dmp( "NO" );
+    }
+private:
+    i64 n;
+    v<ei64>& e;
+    w<i64> W;
+    w<i64> adj;
+    v<bool> src;
+    minpq<pi64> h;
+    vi64 dist;
+    vi64 parent;
+    vi64 count;
+    bool dijk() {
+        while( !h.empty()){
+            auto [d,u] = h.top(); h.pop();
+            if( d > dist[u] ){ continue; }
+            for( auto v: adj[u] ){
+                if( dist[v] > dist[u] + W[u][v] ){
+                    dist[v] = dist[u] + W[u][v];
+                    parent[v] = u;
+                    if( ++count[v] >= n ){
+                        dumpcycle( v );
+                        return true;
+                    }
+                    h.push({ dist[v], v });
+                }
+            }
+        }
+        return false;
+    }
+    void dumpcycle( i64 v ){
+        vi64 cycle{ v };
+        loop {
+            v = parent[v];
+            if( v == 0 ){
+                dmp( "NO" );
+                return;
+            }
+            auto it = find( on( cycle ), v );
+            if( it != cycle.end() ){
+                dbg( cycle );
+                cycle = vi64( it, cycle.end() );
+                cycle.psh( v );
+                break;
+            }
+            cycle.psh( v );
+        } 
+        dmp( "YES" );
+        Rev( j, siz( cycle ) - 1, -1 ){
+            cout << cycle[j] << sp;
+        }
+    }
+};
+/////////////////////////////////////////////////// /  /////////
+////////////////////////////////////////////////// /  //////////
+///////////////////////////////////////////////// /  ///////////
+//////////////////////////////////////////////// /  ////////////
+int main() {
+    vrooom();
+    if( 0 ){
+    } else {
+        auto n = load_scalar<i64>();
+        auto m = load_scalar<i64>();
+        auto e = load_vector<ei64>( m );
+        Algorithm( n, e ).pusk();
+    }
+}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////

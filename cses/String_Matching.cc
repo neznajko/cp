6335e8cdbcf7c716f7592885af27172e944c9628
we4er <neznajko@yahoo.com>
2026-04-04 06:30:17 +0300

    hashing

---
+++ cses/String_Matching.cc
@@ -0,0 +1,305 @@
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//# define DeBug
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# include <bits/stdc++.h>
+////////////////////////////////////////////////////////////////
+using namespace std;
+////////////////////////////////////////////////////////////////
+template <typename T>
+using v = vector<T>;
+template <typename T>
+using w = v<v<T>>;
+template <typename K, typename V>
+using p = pair<K,V>;
+template <typename T>
+using e = tuple<T,T,T>;
+template <typename K, typename V>
+using umap = unordered_map<K,V>;
+template <typename T>
+using uset = unordered_set<T>;
+template <typename T>
+using mset = multiset<T>;
+template <typename T, typename Compare=less<T>>
+using maxpq = priority_queue<T, v<T>, Compare>;
+template <typename T, typename Compare=greater<T>>
+using minpq = priority_queue<T, v<T>, Compare>;
+# ifdef __GNUC__
+# include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <typename T, typename Compare=less<T>>
+using iset = tree<T, null_type, Compare, rb_tree_tag,
+                  tree_order_statistics_node_update>;
+# define FBO find_by_order
+# define OOK order_of_key
+# endif
+////////////////////////////////////////////////////////////////
+using  u8 = unsigned char;
+using i32 = int;
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+using str = string;
+using chr = char;
+using dbl = double;
+////////////////////////////////////////////////////////////////
+using vi32 = v<i32>;
+using vu32 = v<u32>;
+using vi64 = v<i64>;
+using vu64 = v<u64>;
+using pi32 = p<i32,i32>;
+using pu32 = p<u32,u32>;
+using pi64 = p<i64,i64>;
+using pu64 = p<u64,u64>;
+using ei64 = e<i64>;
+////////////////////////////////////////////////////////////////
+constexpr chr nl = '\n';
+constexpr chr sp = ' ';
+constexpr chr tab = '\t';
+constexpr u64 mod1 = 1'000'000'007;
+constexpr i64 inf = 1'000'000'000'000'000;
+constexpr i32 mini32 = numeric_limits<i32>::min();
+constexpr i32 maxi32 = numeric_limits<i32>::max();
+constexpr i64 mini64 = numeric_limits<i64>::min();
+constexpr i64 maxi64 = numeric_limits<i64>::max();
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+inline void vrooom() {
+    cin.tie( nullptr )->sync_with_stdio( false );
+}
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+istream& operator>>( istream& is, p<K,V>& p ){
+    return is >> p.first >> p.second;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+istream& operator>>( istream& is, e<T>& t) {
+    return is >> get<0>( t ) >> get<1>( t ) >> get<2>( t );
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+istream& operator>>( istream& is, v<T>& v ){
+    for( auto& y: v ){ is >> y; }
+    return is;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline T load_scalar() {
+    T y; cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline v<T> load_vector( u32 n ){
+    v<T> y( n ); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline w<T> load_tensor( u32 n, u32 m ){
+    w<T> y( n, v<T>( m )); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename C>
+auto acc( const C& c ){
+    using T = typename C::value_type;
+    return accumulate( c.begin(), c.end(), T( 0 ));
+}
+////////////////////////////////////////////////////////////////
+struct {
+    str open_bracket;
+    str close_bracket;
+    str separator;
+} CONT{ "[", "]", "," },
+  PAIR{ "(", ")", ":" };
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# define FOR_THE_HORDE auto t = load_scalar<u32>(); while( t-- )
+# define loop while( true )
+# define psh push_back
+# define kpop pop_back
+# define emp emplace_back
+# define in push_back
+# define out pop_front
+# define mkp make_pair
+# define fst first
+# define snd second
+# define L lower_bound
+# define U upper_bound
+# define For( i, begin, end ) for( i64 i = begin; i < end; ++i )
+# define Rev( i, end, begin ) for( i64 i = end; i > begin; --i )
+# define odd( x ) ((x) & 1)
+# define evn( y ) (!odd( y ))
+# define sqr( x ) ((x) * (x))
+# define all( x ) x.begin(), x.end()
+# define siz( y ) (i64) y.size()
+#ifdef DeBug
+    template<typename T>
+    inline void debug( string name, T x, string sep="\n" ){
+        cerr << name << " = " << x << sep;
+    }
+    #define dbg( x, ... ) debug( #x, x, ##__VA_ARGS__ )
+#else
+    #define dbg( x, ... )
+#endif
+# define dmp( y ) cout << y << nl
+# define key( p ) p.first
+# define val( p ) p.second
+# define on( y ) all( y )
+////////////////////////////////////////////////////////////////
+# define OVERLOAD_OSTREAM_OPERATOR( Container )                \
+template <typename... T>                                       \
+ostream& operator<<( ostream& os, const Container<T...>& con ){\
+    os << CONT.open_bracket;                                   \
+    str sep = "";                                              \
+    for( const auto& y: con ){                                 \
+        os << sep << y;                                        \
+        sep = CONT.separator;                                  \
+    }                                                          \
+    return os << CONT.close_bracket;                           \
+}
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+ostream& operator<<( ostream& os, const p<K,V>& p );          
+////////////////////////////////////////////////////////////////
+template <typename T>
+ostream& operator<<( ostream& os, const e<T>& t );
+////////////////////////////////////////////////////////////////
+OVERLOAD_OSTREAM_OPERATOR( vector );
+OVERLOAD_OSTREAM_OPERATOR( set );
+OVERLOAD_OSTREAM_OPERATOR( multiset );
+OVERLOAD_OSTREAM_OPERATOR( unordered_map );
+OVERLOAD_OSTREAM_OPERATOR( deque );
+OVERLOAD_OSTREAM_OPERATOR( unordered_set );
+OVERLOAD_OSTREAM_OPERATOR( map );
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+ostream& operator<<( ostream& os, const p<K,V>& p ){
+    return os << PAIR.open_bracket
+              << key( p )
+              << PAIR.separator
+              << val( p )
+              << PAIR.close_bracket;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+ostream& operator<<( ostream& os, const e<T>& t) {
+    return os << "{" << get<0>( t )
+              << "," << get<1>( t )
+              << "," << get<2>( t )
+              << "}";
+}
+////////////////////////////////////////////////////////////////
+#define dmp2(a,b)     cout << a << sp << b << nl;
+#define dbg2(a,b)     dbg(a,", ");dbg(b)
+#define dbg3(a,b,c)   dbg(a,", ");dbg(b,", ");dbg(c)
+#define dbg4(a,b,c,d) dbg(a,", ");dbg(b,", ");dbg(c,", ");dbg(d)
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+enum { BLACK, WHITE, GRAY, RED, BLUE, ORANGE, MAGENTA, BROWN };
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////................
+////////////////////////////////////////////////................
+////////////////////////////////////////////////................
+////////////////////////////////////////////////................
+class Algorithm {
+private:
+    static constexpr u64 A = 911382323;
+    static constexpr u64 B = 972663749;
+    
+    string s, key;
+    vu64   h, p;
+    u64    g;
+
+    void preprocess() {
+        h.resize( siz( s ));
+        p.resize( siz( s ));
+        h[0] = s[0];
+        p[0] = 1;
+        for( u32 j = 1; j < siz( s ); ++j ){
+            h[j] = (h[j - 1]*A + s[j]) % B;
+            p[j] = (p[j - 1]*A) % B;
+        }
+        dbg( h );
+        dbg( p );
+        g = key[0];
+        for( u32 k = 1; k < siz( key ); ++k ){
+            g = (g*A + key[k]) % B;
+        }
+        dbg( g );
+    }
+    u64 hsub( u32 a, u32 b ){
+        if( a == 0 ){ return h[b]; }
+        return (h[b] - h[a - 1]*p[b - a + 1] % B + B) % B;
+    }
+public:
+    void pusk() {
+        cin >> s >> key;
+        preprocess();
+        dbg( s ); dbg( key );
+        u32 n = 0;
+        for( u32 k = 0; k + siz( key ) - 1 < siz( s ); ++k ){
+            auto a = k;
+            auto b = k + siz( key ) - 1;
+            n += ( g == hsub( a, b ));
+        }
+        dmp( n );
+    }
+};
+/////////////////////////////////////////////////// /  /////////
+////////////////////////////////////////////////// /  //////////
+///////////////////////////////////////////////// /  ///////////
+//////////////////////////////////////////////// /  ////////////
+int main() {
+    vrooom();
+    if( 0 ){
+    } else { Algorithm().pusk(); }}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+// String Matching
+//
+// Time limit: 1.00 s
+// Memory limit: 512 MB
+// 
+// Given a string and a pattern, your task is to count the number
+// of positions where the pattern occurs in the string.
+// 
+// Input
+// The first input line has a string of length n, and the second 
+// input line has a pattern of length m. Both of them consist of 
+// characters a–z.
+//
+// Output
+// Print one integer: the number of occurrences.
+// 
+// Constraints
+// 1 <= n,m <= 1e6
+// 
+// Example
+// Input:
+// saippuakauppias
+// pp
+// 
+// Output:
+// 2
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+

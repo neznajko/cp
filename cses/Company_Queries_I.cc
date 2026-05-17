13fcca38690a6ca680f0b59345f506076fb36450
we4er <neznajko@yahoo.com>
2026-05-17 21:11:53 +0300

    compact representation

---
+++ cses/Company_Queries_I.cc
@@ -0,0 +1,371 @@
+// -*- c-file-style: "gnu" -*-
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+// # define DeBug
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# include <bits/stdc++.h>
+////////////////////////////////////////////////////////////////
+using namespace std;
+////////////////////////////////////////////////////////////////
+template <typename T>
+using v = vector<T>;
+template <typename T>
+using w = v<v<T>>;
+template <typename K, typename V>
+using p = pair<K,V>;
+template <typename T>
+using e = tuple<T,T,T>;
+template <typename K, typename V>
+using umap = unordered_map<K,V>;
+template <typename T>
+using uset = unordered_set<T>;
+template <typename T>
+using mset = multiset<T>;
+template <typename T, typename Compare=less<T>>
+using maxpq = priority_queue<T, v<T>, Compare>;
+template <typename T, typename Compare=greater<T>>
+using minpq = priority_queue<T, v<T>, Compare>;
+# ifdef __GNUC__
+# include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <typename T, typename Compare=less<T>>
+using iset = tree<T, null_type, Compare, rb_tree_tag,
+                  tree_order_statistics_node_update>;
+# define FBO find_by_order
+# define OOK order_of_key
+# endif
+////////////////////////////////////////////////////////////////
+using  u8 = unsigned char;
+using i32 = int;
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+using str = string;
+using chr = char;
+using dbl = double;
+////////////////////////////////////////////////////////////////
+using vi32 = v<i32>;
+using vu32 = v<u32>;
+using vi64 = v<i64>;
+using vu64 = v<u64>;
+using pi32 = p<i32,i32>;
+using pu32 = p<u32,u32>;
+using pi64 = p<i64,i64>;
+using pu64 = p<u64,u64>;
+using ei64 = e<i64>;
+////////////////////////////////////////////////////////////////
+constexpr chr nl = '\n';
+constexpr chr sp = ' ';
+constexpr chr tab = '\t';
+constexpr u64 mod1 = 1'000'000'007;
+constexpr i64 inf = 1'000'000'000'000'000;
+constexpr i32 mini32 = numeric_limits<i32>::min();
+constexpr i32 maxi32 = numeric_limits<i32>::max();
+constexpr i64 mini64 = numeric_limits<i64>::min();
+constexpr i64 maxi64 = numeric_limits<i64>::max();
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+inline void vrooom() {
+    cin.tie( nullptr )->sync_with_stdio( false );
+}
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+istream& operator>>( istream& is, p<K,V>& p ){
+    return is >> p.first >> p.second;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+istream& operator>>( istream& is, e<T>& t) {
+    return is >> get<0>( t ) >> get<1>( t ) >> get<2>( t );
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+istream& operator>>( istream& is, v<T>& v ){
+    for( auto& y: v ){ is >> y; }
+    return is;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline T loads() {
+    T y; cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline v<T> loadv( u32 n ){
+    v<T> y( n ); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline w<T> loadt( u32 n, u32 m ){
+    w<T> y( n, v<T>( m )); cin >> y;
+    return y;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+inline p<T,T> loadp() {
+    p<T,T> y;
+    cin >> get<0>( y ) >> get<1>( y );
+    return y;
+}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+template <typename C>
+auto acc( const C& c ){
+    using T = typename C::value_type;
+    return accumulate( c.begin(), c.end(), T( 0 ));
+}
+////////////////////////////////////////////////////////////////
+struct {
+    str open_bracket;
+    str close_bracket;
+    str separator;
+} CONT{ "[", "]", " " },
+  PAIR{ "(", ")", ":" };
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+# define FOR_THE_HORDE auto t = loads<u32>(); while( t-- )
+# define loop while( true )
+# define psh push_back
+# define posh push_back
+# define kpop pop_back
+# define emp emplace_back
+# define in push_back
+# define out pop_front
+# define mkp make_pair
+# define fst first
+# define snd second
+# define L lower_bound
+# define U upper_bound
+# define For( i, begin, end ) for( i64 i = begin; i < end; ++i )
+# define Rev( i, end, begin ) for( i64 i = end; i > begin; --i )
+# define odd( x ) ((x) & 1)
+# define evn( y ) (!odd( y ))
+# define sqr( x ) ((x) * (x))
+# define all( x ) x.begin(), x.end()
+# define siz( y ) (i64) y.size()
+# define dmp( y ) cout << y << nl
+# define key( p ) p.first
+# define val( p ) p.second
+# define on( y ) all( y )
+////////////////////////////////////////////////////////////////
+# define OVERLOAD_OSTREAM_OPERATOR( Container )                \
+template <typename... T>                                       \
+ostream& operator<<( ostream& os, const Container<T...>& con ){\
+    os << CONT.open_bracket;                                   \
+    str sep = "";                                              \
+    for( const auto& y: con ){                                 \
+        os << sep << y;                                        \
+        sep = CONT.separator;                                  \
+    }                                                          \
+    return os << CONT.close_bracket;                           \
+}
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+ostream& operator<<( ostream& os, const p<K,V>& p );          
+////////////////////////////////////////////////////////////////
+template <typename T>
+ostream& operator<<( ostream& os, const e<T>& t );
+////////////////////////////////////////////////////////////////
+OVERLOAD_OSTREAM_OPERATOR( vector );
+OVERLOAD_OSTREAM_OPERATOR( set );
+OVERLOAD_OSTREAM_OPERATOR( multiset );
+OVERLOAD_OSTREAM_OPERATOR( unordered_map );
+OVERLOAD_OSTREAM_OPERATOR( deque );
+OVERLOAD_OSTREAM_OPERATOR( unordered_set );
+OVERLOAD_OSTREAM_OPERATOR( map );
+////////////////////////////////////////////////////////////////
+template <typename K, typename V>
+ostream& operator<<( ostream& os, const p<K,V>& p ){
+    return os << PAIR.open_bracket
+              << key( p )
+              << PAIR.separator
+              << val( p )
+              << PAIR.close_bracket;
+}
+////////////////////////////////////////////////////////////////
+template <typename T>
+ostream& operator<<( ostream& os, const e<T>& t) {
+    return os << "{" << get<0>( t )
+              << "," << get<1>( t )
+              << "," << get<2>( t )
+              << "}";
+}
+////////////////////////////////////////////////////////////////
+#define dmp2(a,b)     cout << a << sp << b << nl;
+#define dbg2(a,b)     dbg(a,", ");dbg(b)
+#define dbg3(a,b,c)   dbg(a,", ");dbg(b,", ");dbg(c)
+#define dbg4(a,b,c,d) dbg(a,", ");dbg(b,", ");dbg(c,", ");dbg(d)
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+enum { BLACK, WHITE, GRAY, RED, BLUE, ORANGE, MAGENTA, BROWN };
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////................
+////////////////////////////////////////////////................
+////////////////////////////////////////////////................
+////////////////////////////////////////////////................
+//##############################################################
+template <typename T>
+ostream& operator<<( ostream& s, const w<T>& mat ){
+    for( auto& row: mat ){ s << nl << row; }
+    return s;
+}
+////////////////////////////////////////////////////////////////
+#ifdef DeBug
+    template<typename T>
+    inline void debug( string name, T x, string sep="\n" ){
+        cerr << name << " = " << x << sep;
+    }
+    #define dbg( x, ... ) debug( #x, x, ##__VA_ARGS__ )
+#else
+    #define dbg( x, ... )
+#endif
+//##############################################################
+//////////////////////////////////////////////////////
+using paint_t = pair<int,int>; // comes in paints
+//////////////////////////////////////////////////////
+struct Algorithm {
+     i32 n;
+  w<i32> c,
+         p;
+
+  // this is kind of a modification of the canonical binary
+  // lifting i guess but is kind of a messy so for every node u
+  // there is a list of parents p[u] such that p[u][j] is the
+  // 2^j-th parent
+  //
+  void levlup( i32 a ){
+    i32 j = 0;
+    while( !p[a].empty()){
+      auto x = p[a].back();
+      if( j >= siz( p[x] )){ break; }
+      p[a].psh( p[x][j++] );
+    }
+  }
+  void dfs() {
+    v<i32> stk( 1, 1 );
+    while( !stk.empty()){
+      auto u = stk.back(); stk.kpop();
+      levlup( u );
+      for( auto v: c[u] ){
+        stk.push_back( v );
+      }
+    }
+  }
+  void bld() {
+    c.resize( n + 1 );
+    p.resize( n + 1 );
+    for( i32 j = 2; j <= n; ++j ){
+      p[ j ].push_back( loads<i32>());
+      c[ p[ j ].back()].push_back( j );
+    }
+    dbg( c ); dbg( p );
+    dfs();
+    dbg( p );
+  }
+  // f( k ) -> j,r
+  // 2^j + r = k
+  // k = 20 -> j=4,r=4
+  static paint_t f( i32 k ){
+    i32 j = 31 - __builtin_clz( k );
+    i32 r = k - (1 << j);
+    return {j,r};
+  }
+
+# define T true
+  i32 query( i32 x, i32 k ){
+    while( T ){
+      auto [j,r] = f( k );
+      dbg2( j, r );
+      if( j >= siz( p[x] )){ return -1; }
+      if( r == 0 ){
+        return p[x][j];
+      }
+      x = p[x][j];
+      k = r;
+    }
+  }
+  void pusk() {                                     
+    n = loads<i32>(); 
+    auto q = loads<i32>();
+    bld();
+    while( q-- ){
+      auto [x,k] = loadp<i32>();
+      dmp( query( x, k ));
+    }
+  }
+};
+///////////////////////////////////////// /  /////////
+//////////////////////////////////////// /  //////////
+/////////////////////////////////////// /  ///////////
+////////////////////////////////////// /  ////////////
+int main() {
+  vrooom();
+  if( 0 ){
+  } else { Algorithm().pusk(); }}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+// Company Queries I
+//
+// Time limit: 1.00 s
+// Memory limit: 512 MB
+// 
+// A company has n employees, who form a tree hierarchy where 
+// each employee has a boss, except for the general director.
+// Your task is to process q queries of the form: who is 
+// employee x's boss k levels higher up in the hierarchy?
+//
+// [ Input ]
+// The first input line has two integers n and q: the number of
+// employees and queries. The employees are numbered 1,2,...,n,
+// and employee 1 is the general director.
+//
+// The next line has n-1 integers e_2,e_3,...,e_n: for each
+// employee 2,3,...,n their boss. Finally, there are q lines
+// describing the queries. Each line has two integers x and k:
+// who is employee x's boss k levels higher up? 
+//
+// [ Output ]
+// Print the answer for each query. If such a boss does not
+// exist, print -1.
+//
+// [ Constraints ]
+// 1 <= n,q <= 2e5
+// 1 <= e_i <= i-1
+// 1 <= x <= n
+// 1 <= k <= n
+// 
+// [ Example ]
+// Input:
+// 5 3
+// 1 1 3 3
+// 4 1
+// 4 2
+// 4 3
+// 
+// Output:
+// 3
+// 1
+// -1
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
